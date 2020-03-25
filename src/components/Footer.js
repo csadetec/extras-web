fec3b73bee6fe908b9018edd9a7cdda211eea675
csadetec <informaticact@santoagostinho.com.br>
2020-03-24 21:39:12 -0300

    download meia boka

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Footer() {
   return ( 
     <footer className="page-footer font-small fixed-bottom green  ">
-      <div className="footer-copyright text-center py-1">© 2020: Comeve
+      <div className="footer-copyright text-center py-1">© 2020: Serviços Extras
        
       </div>
     </footer>
--- src/pages/ReportList.js
+++ src/pages/ReportList.js
@@ -1,25 +1,70 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
 import { formatDate } from '../utils/helpers'
-
+import api from '../service/api'
+import {saveAs} from 'file-saver'
 
 function ReportList() {
-	const [reports, setReports] = useState(JSON.parse(localStorage.getItem('reports')))
+	const [reports] = useState(JSON.parse(localStorage.getItem('reports')))
 	//console.log(users)
+	const [date, setDate] = useState({ start: '2020-01-01', end: '2020-12-12' })
+
 	let cont = 1
 	useEffect(() => {
 		document.title = 'Relatório'
+
+		const date = new Date()
+
+		const day = date.getDay()
+		console.log(day)
+
 		//console.log(reports)
 	})
-	return (
-		<div className="container" >
-			<div className="row mb-3">
-				<div className="col-md-12 border-bottom">
-					<Link type="button" className="btn btn-green float-right" to='/usuarios/cadastrar'>Cadastrar Usuário</Link>
 
+	const inputStyle = {
+		border: 'none',
+		borderBottom: '2px solid #495057',
+		borderRadius: 0,
+		marginLength: '3px',
+		paddingTop: '1px'
+		/** */
+	}
+	const updateField = ({ target: { name, value } }) => {
+		setDate({ ...date, [name]: value })
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		api.post('/pdf', date)
+			.then(() => api.get('/pdf', {responseType:'blob'}))
+			.then((res) => {
+				const pdfBlob = new Blob([res.data], {type: 'application/pdf'})
+				saveAs(pdfBlob, `${date.start} - ${date.end}.pdf`)
+			})
+
+	}
+
+	return (
+		<div className="container " >
+			<div className="row justify-content mb-3 border-bottom">
+				<div className="col-md-6">
 					<h2>Relatório</h2>
+
+				</div>
+				<div className="col-md-6">
+					<form onSubmit={handleSubmit} className="form-inline">
+						<div className="form-row ml-5">
+								<input type="date" className="form-control" style={inputStyle} name="start"
+									value={date.start} onChange={updateField} required />
+							-	<input type="date" className="form-control" style={inputStyle} name="end"
+									value={date.end} onChange={updateField} required />
+
+						</div>
+						<button type="submit" className="btn btn-outline-danger float-right ml-3" >Gerar PDF</button>
+					</form>
+
 				</div>
 			</div>
+
 			<div className="row">
 				<div className="col-md-12">
 					<table className="table">
@@ -36,7 +81,7 @@ function ReportList() {
 						<tbody>
 
 							{reports.map(r =>
-								<tr key={r.id} >
+								<tr key={cont++} >
 									<th>{cont++}</th>
 									<td>{r.employee.name} | {r.employee.id}</td>
 									<td>{r.reason_name}</td>
