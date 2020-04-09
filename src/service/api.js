49baabd06bbf2fc77913e671e4d00b54c5e71fd0
csadetec <informaticact@santoagostinho.com.br>
2020-04-09 13:07:09 -0300

    add api azure

--- src/service/api.js
+++ src/service/api.js
@@ -2,16 +2,17 @@ import axios from 'axios'
 
 const hostname = () => {
   const app = window.location.hostname 
-  if(app === '10.20.0.22'){
+  if(app === '10.20.0.22')
     return 'http://10.20.2.22:8000'
-  }
-  if(app === 'localhost'){
+  
+  if(app === 'localhost')
     return 'http://localhost:8000' 
-  }
-  if(app === 'extras-teste.netlify.com'){
+  
+  if(app === 'extras-teste.netlify.com')
     return 'https://extras.detec.site'
-  }
-
+  
+  if(app === 'extras-azure.netlify.com')
+     return 'https://extras-api.azurewebsites.net'
   //https://comeve.netlify.com/
   return 'http://10.20.0.22:8000'
 }
