b616f8fd5689aafcb33949794ee343ec574b8bac
csadetec <informaticact@santoagostinho.com.br>
2020-04-12 16:05:15 -0300

    logout clear

--- src/utils/logout.js
+++ src/utils/logout.js
@@ -2,9 +2,10 @@ const logout = () => {
   
   localStorage.clear()
   window.location.reload()
-  /** */
+  /** *
   window.alert('Deu algum error\nFazer Login novamente')
   console.log('error logout')
+  /** */
 }
 
 export default logout
